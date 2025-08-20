a70389c83562bdbbdf538d5dba1b1f7c437acb23
Ridam-Mittal <ridam112004@gmail.com>
2025-08-20 17:45:33 +0530

    updated client.js

--- backend/inngest/client.js
+++ backend/inngest/client.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-  name: "resume-analysis-system",       
+  id: "resume-analysis-system",       
   auth: process.env.INNGEST_EVENT_KEY, 
 });
